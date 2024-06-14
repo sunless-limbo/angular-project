bdcd05152c9ecd0a36ab8a3efb20057012a08459
sunless13 <sunlesskelv@gmail.com>
2024-06-14 18:12:41 +0300

    edit project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PatientTwo
+# Angular Project
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.3.
 
---
+++ src/app/article-content.ts
@@ -0,0 +1,8 @@
+export interface ArticleContent {
+  id: number;
+  image: string;
+  topic: string;
+  time: string;
+  title: string;
+  summary: string;
+}
