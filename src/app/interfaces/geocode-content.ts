f9e6b09972d956aa031ed62819fdc47c4ea37ddb
sunless13 <sunlesskelv@gmail.com>
2024-06-21 07:32:33 +0300

    feat: Added Search Button
    doesn't work for now, but will do

---
+++ src/app/interfaces/geocode-content.ts
@@ -0,0 +1,7 @@
+export interface GeocodeContent {
+  name: string;
+  country: string;
+  latitude: number;
+  longitude: number;
+  state: string;
+}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -9,12 +9,32 @@ import { WeatherApiService } from '../services/weather-api.service';
   styleUrl: './weather.component.scss',
 })
 export class WeatherComponent implements OnInit {
+  weatherPerSearch: string = '';
+  iconCodePerSearch: string = '';
+  iconUrlPerSearch: string = '';
+
   geocodeData: string = '';
   weatherData: string = '';
   iconCode: string = '';
   iconUrl: string = '';
 
   constructor(private weatherApiService: WeatherApiService) {}
+
+  // weather per the search data
+  weatherResults = () => {
+    this.weatherApiService.getWeather().subscribe((res) => {
+      this.iconCodePerSearch = `${res.weather[0].icon}`;
+      this.iconUrlPerSearch = `http://openweathermap.org/img/wn/${this.iconCode}@2x.png`;
+      this.weatherPerSearch = `
+        ${res.weather[0].main}
+        ${res.clouds.all}%
+        ${res.weather[0].description}
+        ${res.main.temp} degrees
+        ${res.main.pressure} pressure degrees
+        ${res.main.humidity} humidity degrees
+      `;
+    });
+  };
 
   ngOnInit(): void {
     // geocoding data
@@ -26,13 +46,12 @@ export class WeatherComponent implements OnInit {
         ${res[0].lon}
         ${res[0].state}
       `;
-    });
-
-    // weather data
-    this.weatherApiService.getWeather().subscribe((res) => {
-      this.iconCode = `${res.weather[0].icon}`;
-      this.iconUrl = `http://openweathermap.org/img/wn/${this.iconCode}@2x.png`;
-      this.weatherData = `
+    }),
+      // weather data
+      this.weatherApiService.getWeather().subscribe((res) => {
+        this.iconCode = `${res.weather[0].icon}`;
+        this.iconUrl = `http://openweathermap.org/img/wn/${this.iconCode}@2x.png`;
+        this.weatherData = `
         ${res.weather[0].main}
         ${res.clouds.all}%
         ${res.weather[0].description}
@@ -40,6 +59,6 @@ export class WeatherComponent implements OnInit {
         ${res.main.pressure} pressure degrees
         ${res.main.humidity} humidity degrees
       `;
-    });
+      });
   }
 }
